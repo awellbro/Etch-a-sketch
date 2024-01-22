821c5554ace1761a52a8fa8d29c88a066b8dfdf6
Aaron <wellbrockaaron@gmail.com>
2024-01-21 16:30:33 -0800

    still need to adjust grid to fit evenly, if neccessary

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const mkSq = document.querySelector("#squareDivs");
 
 for(let i=0; i<256; i++){
 const newDiv = document.createElement("div");
-newDiv.textContent = ("Here it is");
+newDiv.textContent = ("*");
 
 newDiv.setAttribute("class", "newDiv");
 
