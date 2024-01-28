140b180896e8b87b4f06014cf3a69601e9884b96
Aaron <wellbrockaaron@gmail.com>
2024-01-27 19:54:17 -0800

    user input now sets max width and height

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
-let countPix = prompt("How Many Pixels(Sqred)");
-let newCount = countPix * countPix;
 //selecting the retainer for the pixels to go in
 const mkSq = document.querySelector("#squareDivs");
 
+   let countPix = prompt("Enter a number between 20 and 100");
 
-//creates a 16x16 grid to draw on
+   let newHeight = countPix * 3;
+   let newWidth = countPix * 3;
+   let newCount = countPix* countPix;
+
+   mkSq.style.maxHeight = (`${newHeight}px`);
+   mkSq.style.maxWidth = (`${newWidth}px`);
+
+   console.log(newHeight);
+   console.log(newWidth);
+
+//creates a grid to draw on - based on user input (countPix)
 for(let i=0; i < newCount; i++){
    console.log(newCount);
 const newDiv = document.createElement("div");
@@ -15,12 +24,9 @@ mkSq.appendChild(newDiv);
 };
 
 
+
+//function that creates the drawing effect
 const count = document.querySelectorAll('.newDiv');
-const box = document.querySelector('#squareDivs');
-
-
-const draw = document.querySelector('.newDiv');
-
 count.forEach(function(event){
 event.addEventListener('mouseover', function(x){
    this.style.backgroundColor = "green";
